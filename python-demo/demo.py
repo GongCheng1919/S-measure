f43cff3e48261f20ea15ab57f66b91b4a373e484
gongcheng_nankai@163.com <gongcheng_nankai@163.com>
2019-05-01 20:44:04 +0800

    add python-demo

--- python-demo/demo.py
+++ python-demo/demo.py
@@ -12,17 +12,17 @@ if not os.path.exists(resPath):
 MethodNames = ['MDF','mc','DISC','rfcn','DCL','dhsnet']
 # load the gtFiles
 gtFiles = ListFile(gtPath,'png')
-print(gtFiles)
+#print(gtFiles)
 for item in gtFiles:
 	print('Processing %s...\n'%(item))
 	GT=cv2.imread(item[0]+item[1],0).astype(np.float32)/255.0
-	print(GT.shape,GT.max(),GT.min())
+	#print(GT.shape,GT.max(),GT.min())
 	for method in MethodNames:
 		predname=fgPath+item[1][:-4]+"_"+method+".png"
-		print(predname)
+		#print(predname)
 		prediction=cv2.imread(predname,0).astype(np.float32)
 		prediction=(prediction-prediction.min())/(prediction.max()-prediction.min())
-		print(prediction.shape,prediction.max(),prediction.min())
+		#print(prediction.shape,prediction.max(),prediction.min())
 		score=StructureMeasure(prediction,GT)
 		resName=resPath+item[1][:-4]+"_%0.4f_"%(score)+method+".png"
 		cv2.imwrite(resName,(prediction*255).astype(np.uint8))
